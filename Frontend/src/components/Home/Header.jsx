bc137aa7b0e732ee49930dd972588c1b002bba27
FahdAzmy <azmyfahd66@gmail.com>
2024-12-23 16:34:02 +0200

    try ci/cd pipline

--- Frontend/src/components/Home/Header.jsx
+++ Frontend/src/components/Home/Header.jsx
@@ -6,9 +6,7 @@ const Navbar = () => {
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         <div className="flex items-center">
           <TrendingUp className="text-blue-600 mr-2" size={30} />
-          <h1 className="text-2xl font-bold text-gray-800">
-            Financial Horizon
-          </h1>
+          <h1 className="text-2xl font-bold text-gray-800">Financial</h1>
         </div>
         <div className="space-x-4">
           <a href="#" className="text-gray-600 font-bold hover:text-blue-600">
